535807e59672d12aea8bb427401294348e6dd436
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-01 12:38:51 +0100

    done task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+Flask web application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/number_template/<int:n>")
+def number_template_route(n):
+    """
+    display a HTML page only if n is an integer
+    """
+    if isinstance(n, int):
+        return render_template("5-number.html", num=n)
+
+
+@app.route("/number/<int:n>")
+def number_route(n):
+    """
+    display "n is a number" only if n is an integer
+    """
+    if isinstance(n, int):
+        return str(n) + " is a number"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_route(text=None):
+    """
+    display “Python ”, followed by the value of the text
+    """
+    if text:
+        q = ' '.join(text.split('_'))
+    else:
+        q = 'is cool'
+    return "Python " + q
+
+
+@app.route("/c/<text>")
+def c_route(text):
+    """
+    display "C" followed by the value of the text variable
+    """
+    q = ' '.join(text.split('_'))
+    return "C " + q
+
+
+@app.route("/hbnb")
+def hbnb_route():
+    """
+    return HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/")
+def hello_route():
+    """
+    return page with message
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
